05656dc4c9a1871c16f83c0a2d2d0a7310d95dd0
Kris Baumgartner <kjbaumgartner@gmail.com>
2024-05-29 22:07:19 -0700

    Add Scheduler class

---
+++ packages/scheduler/src/class/utils/create-options-fns.ts
@@ -0,0 +1,43 @@
+import {
+    after as afterFn,
+    before as beforeFn,
+    id as idFn,
+    tag as tagFn,
+} from '../../scheduler';
+import { OptionsFn } from '../../scheduler-types';
+import { OptionsObject } from '../types';
+
+export function createOptionsFns(options: OptionsObject | undefined) {
+    const optionsFns: OptionsFn[] = [];
+
+    if (options?.id) {
+        optionsFns.push(idFn(options.id));
+    }
+
+    if (options?.before) {
+        if (Array.isArray(options.before)) {
+            optionsFns.push(
+                ...options.before.map((before) => beforeFn(before))
+            );
+        } else {
+            optionsFns.push(beforeFn(options.before));
+        }
+    }
+
+    if (options?.after) {
+        if (Array.isArray(options.after)) {
+            optionsFns.push(...options.after.map((after) => afterFn(after)));
+        } else {
+            optionsFns.push(afterFn(options.after));
+        }
+    }
+
+    if (options?.tag) {
+        if (Array.isArray(options.tag)) {
+            optionsFns.push(...options.tag.map((tag) => tagFn(tag)));
+        } else {
+            optionsFns.push(tagFn(options.tag));
+        }
+    }
+    return optionsFns;
+}
